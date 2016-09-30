2f45059d0bbdbab3855af34d6657a571224cefe8
Paulo Faria <paulo.faria.rl@gmail.com>
2016-09-30 04:01:41 -0300

    fix serializers

--- Modules/Core/Sources/Core/JSON/JSONMapSerializer.swift
+++ Modules/Core/Sources/Core/JSON/JSONMapSerializer.swift
@@ -1,10 +1,8 @@
-// This file has been modified from its original project Swift-JsonSerializer
-
 public final class JSONMapSerializer : MapSerializer {
     private var ordering: Bool
     private var buffer: String = ""
     private var bufferSize: Int = 0
-    private var body: (UnsafeBufferPointer<Byte>) throws -> Void = { _ in }
+    private typealias Body = (UnsafeBufferPointer<Byte>) throws -> Void
 
     public init() {
         self.ordering = false
@@ -14,63 +12,63 @@ public final class JSONMapSerializer : MapSerializer {
         self.ordering = ordering
     }
 
-    public func serialize(_ map: Map, bufferSize: Int, body: (UnsafeBufferPointer<Byte>) throws -> Void) throws {
+    public func serialize(_ map: Map, bufferSize: Int, body: Body) throws {
         self.bufferSize = bufferSize
-        try serialize(value: map)
-        try write()
+        try serialize(value: map, body: body)
+        try write(body: body)
     }
 
-    private func serialize(value: Map) throws {
+    private func serialize(value: Map, body: Body) throws {
         switch value {
-        case .null: try append(string: "null")
-        case .bool(let bool): try append(string: String(bool))
-        case .double(let number): try append(string: String(number))
-        case .int(let number): try append(string: String(number))
-        case .string(let string): try serialize(string: string)
-        case .array(let array): try serialize(array: array)
-        case .dictionary(let dictionary): try serialize(dictionary: dictionary)
+        case .null: try append(string: "null", body: body)
+        case .bool(let bool): try append(string: String(bool), body: body)
+        case .double(let number): try append(string: String(number), body: body)
+        case .int(let number): try append(string: String(number), body: body)
+        case .string(let string): try serialize(string: string, body: body)
+        case .array(let array): try serialize(array: array, body: body)
+        case .dictionary(let dictionary): try serialize(dictionary: dictionary, body: body)
         default: throw MapError.incompatibleType
         }
     }
 
-    private func serialize(array: [Map]) throws {
-        try append(string: "[")
+    private func serialize(array: [Map], body: Body) throws {
+        try append(string: "[", body: body)
 
         for index in 0 ..< array.count {
-            try serialize(value: array[index])
+            try serialize(value: array[index], body: body)
 
             if index != array.count - 1 {
-                try append(string: ",")
+                try append(string: ",", body: body)
             }
         }
 
-        try append(string: "]")
+        try append(string: "]", body: body)
     }
 
-    private func serialize(dictionary: [String: Map]) throws {
-        try append(string: "{")
+    private func serialize(dictionary: [String: Map], body: Body) throws {
+        try append(string: "{", body: body)
         var index = 0
 
         if ordering {
             for (key, value) in dictionary.sorted(by: {$0.0 < $1.0}) {
-                try serialize(string: key)
-                try append(string: ":")
-                try serialize(value: value)
+                try serialize(string: key, body: body)
+                try append(string: ":", body: body)
+                try serialize(value: value, body: body)
 
                 if index != dictionary.count - 1 {
-                    try append(string: ",")
+                    try append(string: ",", body: body)
                 }
 
                 index += 1
             }
         } else {
             for (key, value) in dictionary{
-                try serialize(string: key)
-                try append(string: ":")
-                try serialize(value: value)
+                try serialize(string: key, body: body)
+                try append(string: ":", body: body)
+                try serialize(value: value, body: body)
 
                 if index != dictionary.count - 1 {
-                    try append(string: ",")
+                    try append(string: ",", body: body)
                 }
                 
                 index += 1
@@ -78,40 +76,40 @@ public final class JSONMapSerializer : MapSerializer {
         }
 
 
-        try append(string: "}")
+        try append(string: "}", body: body)
     }
 
-    private func serialize(string: String) throws {
-        try append(string: "\"")
+    private func serialize(string: String, body: Body) throws {
+        try append(string: "\"", body: body)
 
         for character in string.characters {
             if let escapedSymbol = escapeMapping[character] {
-                try append(string: escapedSymbol)
+                try append(string: escapedSymbol, body: body)
             } else {
-                try append(character: character)
+                try append(character: character, body: body)
             }
         }
 
-        try append(string: "\"")
+        try append(string: "\"", body: body)
     }
 
-    private func append(character: Character) throws {
+    private func append(character: Character, body: Body) throws {
         buffer.append(character)
 
         if buffer.characters.count >= bufferSize {
-            try write()
+            try write(body: body)
         }
     }
 
-    private func append(string: String) throws {
+    private func append(string: String, body: Body) throws {
         buffer += string
 
         if buffer.characters.count >= bufferSize {
-            try write()
+            try write(body: body)
         }
     }
 
-    private func write() throws {
+    private func write(body: Body) throws {
         try buffer.withCString {
             try $0.withMemoryRebound(to: Byte.self, capacity: buffer.utf8.count) {
                 try body(UnsafeBufferPointer(start: $0, count: buffer.utf8.count))
--- Modules/Core/Sources/Core/URLEncodedForm/URLEncodedFormMapSerializer.swift
+++ Modules/Core/Sources/Core/URLEncodedForm/URLEncodedFormMapSerializer.swift
@@ -5,40 +5,40 @@ enum URLEncodedFormMapSerializerError : Error {
 public final class URLEncodedFormMapSerializer : MapSerializer {
     private var buffer: String = ""
     private var bufferSize: Int = 0
-    private var body: (UnsafeBufferPointer<Byte>) throws -> Void = { _ in }
+    private typealias Body = (UnsafeBufferPointer<Byte>) throws -> Void
 
     public init() {}
 
-    public func serialize(_ map: Map, bufferSize: Int, body: (UnsafeBufferPointer<Byte>) throws -> Void) throws {
+    public func serialize(_ map: Map, bufferSize: Int, body: Body) throws {
         self.bufferSize = bufferSize
 
         switch map {
         case .dictionary(let dictionary):
             for (offset: index, element: (key: key, value: map)) in dictionary.enumerated() {
                 if index != 0 {
-                   try append(string: "&")
+                   try append(string: "&", body: body)
                 }
 
-                try append(string: key + "=")
+                try append(string: key + "=", body: body)
                 let value = try map.asString(converting: true)
-                try append(string: value.percentEncoded(allowing: .uriQueryAllowed))
+                try append(string: value.percentEncoded(allowing: .uriQueryAllowed), body: body)
             }
         default:
             throw URLEncodedFormMapSerializerError.invalidMap
         }
         
-        try write()
+        try write(body: body)
     }
 
-    private func append(string: String) throws {
+    private func append(string: String, body: Body) throws {
         buffer += string
 
         if buffer.characters.count >= bufferSize {
-            try write()
+            try write(body: body)
         }
     }
 
-    private func write() throws {
+    private func write(body: Body) throws {
         try buffer.withCString {
             try $0.withMemoryRebound(to: Byte.self, capacity: buffer.utf8.count) {
                 try body(UnsafeBufferPointer(start: $0, count: buffer.utf8.count))
