117be0a6fce3a95b758fc493154bb01477e7c8a4
Robert Payne <robertpayne@me.com>
2016-10-01 14:50:28 +1300

    Swift JSON serializer to YAJL

--- Modules/Core/Sources/Core/JSON/JSONMapSerializer.swift
+++ Modules/Core/Sources/Core/JSON/JSONMapSerializer.swift
@@ -1,133 +1,178 @@
+#if os(Linux)
+    import Glibc
+#else
+    import Darwin.C
+#endif
+
+import CYAJL
+
+public struct JSONMapSerializerError : Error, CustomStringConvertible {
+    let reason: String
+    
+    public var description: String {
+        return reason
+    }
+}
+
 public final class JSONMapSerializer : MapSerializer {
     private var ordering: Bool
     private var buffer: String = ""
     private var bufferSize: Int = 0
     private typealias Body = (UnsafeBufferPointer<Byte>) throws -> Void
-
-    public init() {
-        self.ordering = false
+    
+    private var handle: yajl_gen!
+    
+    public convenience init() {
+        self.init(ordering: false)
     }
-
+    
     public init(ordering: Bool) {
         self.ordering = ordering
+        self.handle = yajl_gen_alloc(nil)
     }
-
+    
+    deinit {
+        yajl_gen_free(handle)
+    }
+    
     public func serialize(_ map: Map, bufferSize: Int = 4096, body: Body) throws {
+        yajl_gen_reset(handle, nil)
         self.bufferSize = bufferSize
-        try serialize(value: map, body: body)
+        try generate(map, body: body)
         try write(body: body)
     }
-
-    private func serialize(value: Map, body: Body) throws {
+    
+    private func generate(_ value: Map, body: Body) throws {
         switch value {
-        case .null: try append(string: "null", body: body)
-        case .bool(let bool): try append(string: String(bool), body: body)
-        case .double(let number): try append(string: String(number), body: body)
-        case .int(let number): try append(string: String(number), body: body)
-        case .string(let string): try serialize(string: string, body: body)
-        case .array(let array): try serialize(array: array, body: body)
-        case .dictionary(let dictionary): try serialize(dictionary: dictionary, body: body)
-        default: throw MapError.incompatibleType
+        case .null:
+            try generateNull()
+        case .bool(let bool):
+            try generate(bool)
+        case .double(let double):
+            try generate(double)
+        case .int(let int):
+            try generate(int)
+        case .string(let string):
+            try generate(string)
+        case .array(let array):
+            try generate(array, body: body)
+        case .dictionary(let dictionary):
+            try generate(dictionary, body: body)
+        default:
+            throw MapError.incompatibleType
         }
+        
+        try write(highwater: bufferSize, body: body)
     }
-
-    private func serialize(array: [Map], body: Body) throws {
-        try append(string: "[", body: body)
-
-        for index in 0 ..< array.count {
-            try serialize(value: array[index], body: body)
-
-            if index != array.count - 1 {
-                try append(string: ",", body: body)
-            }
-        }
-
-        try append(string: "]", body: body)
-    }
-
-    private func serialize(dictionary: [String: Map], body: Body) throws {
-        try append(string: "{", body: body)
-        var index = 0
-
+    
+    private func generate(_ dictionary: [String: Map], body: Body) throws {
+        var status = yajl_gen_status_ok
+        
+        status = yajl_gen_map_open(handle)
+        try check(status: status)
+        
         if ordering {
-            for (key, value) in dictionary.sorted(by: {$0.0 < $1.0}) {
-                try serialize(string: key, body: body)
-                try append(string: ":", body: body)
-                try serialize(value: value, body: body)
-
-                if index != dictionary.count - 1 {
-                    try append(string: ",", body: body)
-                }
-
-                index += 1
+            for key in dictionary.keys.sorted() {
+                try generate(key)
+                try generate(dictionary[key]!, body: body)
             }
         } else {
-            for (key, value) in dictionary{
-                try serialize(string: key, body: body)
-                try append(string: ":", body: body)
-                try serialize(value: value, body: body)
-
-                if index != dictionary.count - 1 {
-                    try append(string: ",", body: body)
+            for (key, value) in dictionary {
+                try generate(key)
+                try generate(value, body: body)
+            }
+        }
+        
+        status = yajl_gen_map_close(handle)
+        try check(status: status)
+    }
+    
+    private func generate(_ array: [Map], body: Body) throws {
+        var status = yajl_gen_status_ok
+        
+        status = yajl_gen_array_open(handle)
+        try check(status: status)
+        
+        for value in array {
+            try generate(value, body: body)
+        }
+        
+        status = yajl_gen_array_close(handle)
+        try check(status: status)
+    }
+    
+    private func generateNull() throws {
+        try check(status: yajl_gen_null(handle))
+    }
+    
+    private func generate(_ string: String) throws {
+        let status: yajl_gen_status
+        
+        if string.isEmpty {
+            status = yajl_gen_string(handle, nil, 0)
+        } else {
+            status = string.withCString { cStringPointer in
+                return cStringPointer.withMemoryRebound(to: UInt8.self, capacity: string.utf8.count) {
+                    yajl_gen_string(self.handle, $0, string.utf8.count)
                 }
-                
-                index += 1
             }
         }
-
-
-        try append(string: "}", body: body)
+        
+        try check(status: status)
     }
-
-    private func serialize(string: String, body: Body) throws {
-        try append(string: "\"", body: body)
-
-        for character in string.characters {
-            if let escapedSymbol = escapeMapping[character] {
-                try append(string: escapedSymbol, body: body)
-            } else {
-                try append(character: character, body: body)
-            }
-        }
-
-        try append(string: "\"", body: body)
+    
+    private func generate(_ bool: Bool) throws {
+        try check(status: yajl_gen_bool(handle, (bool) ? 1 : 0))
     }
-
-    private func append(character: Character, body: Body) throws {
-        buffer.append(character)
-
-        if buffer.characters.count >= bufferSize {
-            try write(body: body)
+    
+    private func generate(_ double: Double) throws {
+        let string = double.description
+        let status = string.withCString { pointer in
+            return yajl_gen_number(self.handle, pointer, string.utf8.count)
+        }
+        try check(status: status)
+    }
+    
+    private func generate(_ int: Int) throws {
+        try check(status: yajl_gen_integer(handle, Int64(int)))
+    }
+    
+    private func check(status: yajl_gen_status) throws {
+        switch status {
+        case yajl_gen_keys_must_be_strings:
+            throw JSONMapSerializerError(reason: "Keys must be strings.")
+        case yajl_max_depth_exceeded:
+            throw JSONMapSerializerError(reason: "Max depth exceeded.")
+        case yajl_gen_in_error_state:
+            throw JSONMapSerializerError(reason: "In error state.")
+        case yajl_gen_invalid_number:
+            throw JSONMapSerializerError(reason: "Invalid number.")
+        case yajl_gen_no_buf:
+            throw JSONMapSerializerError(reason: "No buffer.")
+        case yajl_gen_invalid_string:
+            throw JSONMapSerializerError(reason: "Invalid string.")
+        case yajl_gen_status_ok:
+            break
+        case yajl_gen_generation_complete:
+            break
+        default:
+            throw JSONMapSerializerError(reason: "Unknown.")
         }
     }
-
-    private func append(string: String, body: Body) throws {
-        buffer += string
-
-        if buffer.characters.count >= bufferSize {
-            try write(body: body)
+    
+    private func write(highwater: Int = 0, body: Body) throws {
+        var buffer: UnsafePointer<UInt8>? = nil
+        var bufferLength: Int = 0
+        
+        guard yajl_gen_get_buf(handle, &buffer, &bufferLength) == yajl_gen_status_ok else {
+            throw JSONMapSerializerError(reason: "Could not get buffer.")
         }
-    }
-
-    private func write(body: Body) throws {
-        try buffer.withCString {
-            try $0.withMemoryRebound(to: Byte.self, capacity: buffer.utf8.count) {
-                try body(UnsafeBufferPointer(start: $0, count: buffer.utf8.count))
-            }
+        
+        guard bufferLength >= highwater else {
+            return
         }
-        buffer = ""
+        
+        try body(UnsafeBufferPointer(start: buffer, count: bufferLength))
+        yajl_gen_clear(handle)
     }
 }
-
-fileprivate let escapeMapping: [Character: String] = [
-    "\r": "\\r",
-    "\n": "\\n",
-    "\t": "\\t",
-    "\\": "\\\\",
-    "\"": "\\\"",
-
-    "\u{2028}": "\\u2028",
-    "\u{2029}": "\\u2029",
-
-    "\r\n": "\\r\\n"
-]
