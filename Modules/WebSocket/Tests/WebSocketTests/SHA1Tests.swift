2241c381e397cb5d2f4fcd7de50b113d89811c22
Dan Appel <dan.appel00@gmail.com>
2016-10-12 15:13:05 -0700

    Add SHA1 tests to linuxmain, fix indentation

--- Modules/WebSocket/Tests/WebSocketTests/SHA1Tests.swift
+++ Modules/WebSocket/Tests/WebSocketTests/SHA1Tests.swift
@@ -1,18 +1,17 @@
 import XCTest
 @testable import WebSocket
 
-class SHA1Tests: XCTestCase {
-
+class SHA1Tests : XCTestCase {
     func testSHA1() {
         let data = Array("sha1".utf8)
         let hash = sha1(data)
-        
+
         var hexString = ""
         for byte in hash {
             hexString += String(format: "%02x", UInt(byte))
         }
-        
-        XCTAssert(hexString == "415ab40ae9b7cc4e66d6769cb2c08106e8293b48")
+
+        XCTAssertEqual(hexString, "415ab40ae9b7cc4e66d6769cb2c08106e8293b48")
     }
 
 }
