6cb1ad72fec00ef233d6ea5903177d17fd15500c
Solomenchuk, Vlad <vlad.solomenchuk@sap.com>
2016-10-18 23:24:33 -0700

    another fix for tests

--- Modules/Axis/Tests/AxisTests/URL/URLTests.swift
+++ Modules/Axis/Tests/AxisTests/URL/URLTests.swift
@@ -6,8 +6,15 @@ public class URLTests : XCTestCase {
     func testQueryItems() {
         let url = URL(string: "http://zewo.io?a=b&c=d%20e")!
         let queryItems = url.queryItems
-        XCTAssertEqual(queryItems[0], URLQueryItem(name: "a", value: "b"))
-        XCTAssertEqual(queryItems[1], URLQueryItem(name: "c", value: "d e"))
+        
+        //this is weird. If you run `XCTAssertEqual(URLQueryItem, URLQueryItem)`
+        //just for Axis, everything works, but for Zewo it does not.
+        let v0 = URLQueryItem(name: "a", value: "b")
+        let v1 = URLQueryItem(name: "c", value: "d e")
+        XCTAssertEqual(queryItems[0].name, v0.name)
+        XCTAssertEqual(queryItems[0].value, v0.value)
+        XCTAssertEqual(queryItems[1].name, v1.name)
+        XCTAssertEqual(queryItems[1].value, v1.value)
     }
 }
 
