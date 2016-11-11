6018f50819d2cfd1fc94de4357c0a9127a029950
Dan Appel <dan.appel00@gmail.com>
2016-11-11 11:50:40 -0800

    Update xcode project script (temporary, revert later)

--- Scripts/generate-xcodeproj.sh
+++ Scripts/generate-xcodeproj.sh
@@ -1,3 +1,3 @@
-swift package generate-xcodeproj
+swift package generate-xcodeproj -Xcc -I/usr/local/include -Xlinker -L/usr/local/lib/ -Xswiftc -I/usr/local/include
 sed -i '' -E "s/(path = 'Sources\/)([^\']+)(')/path = 'Modules\/\2\/Sources\/\2'/" Zewo.xcodeproj/project.pbxproj
 sed -i '' -E "s/(path = 'Tests\/)([^\']+)(Tests')/path = 'Modules\/\2\/Tests\/\2Tests'/" Zewo.xcodeproj/project.pbxproj
